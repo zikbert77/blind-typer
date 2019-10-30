4e7562af072d5818a657ee7869038622bf2e4b39
zikbert77 <bogdan@hosteeva.com>
2019-10-30 10:27:51 +0200

    Scroll to test-wrapper on loading test page

--- public/js/test.js
+++ public/js/test.js
@@ -27,3 +27,7 @@ function insertParam(key, value)
     //this will reload the page, it's likely better to store this until finished
     document.location.search = kvp.join('&');
 }
+
+function scrollToElement(ele) {
+    $(window).scrollTop(ele.offset().top).scrollLeft(ele.offset().left);
+}
