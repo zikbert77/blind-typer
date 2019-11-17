0dab4ca4c88abdea89ff3df0991b73ed780aecc1
zikbert77 <bogdan@hosteeva.com>
2019-11-17 15:12:09 +0200

    Reset password entity

---
+++ src/Entity/ResetPasswordRequests.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+class ResetPasswordRequests
+{
+    private $id;
+
+    private $hash;
+
+    private $status;
+
+    private $createdAt;
+
+    private $validTo;
+
+    private $updatedAt;
+
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHash(): ?string
+    {
+        return $this->hash;
+    }
+
+    public function setHash(?string $hash): self
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getValidTo(): ?\DateTimeInterface
+    {
+        return $this->validTo;
+    }
+
+    public function setValidTo(?\DateTimeInterface $validTo): self
+    {
+        $this->validTo = $validTo;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?Users
+    {
+        return $this->user;
+    }
+
+    public function setUser(?Users $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
