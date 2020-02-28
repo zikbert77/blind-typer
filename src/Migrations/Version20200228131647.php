95d59df843b0cf2915e24a3359c1e9fdb0e35d10
zikbert77 <bogdan@hosteeva.com>
2020-02-28 15:28:17 +0200

    added interface_language

---
+++ src/Migrations/Version20200228131647.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200228131647 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users ADD interface_language INT(10) UNSIGNED DEFAULT NULL AFTER default_keyboard');
+        $this->addSql('ALTER TABLE users ADD FOREIGN KEY(interface_language) REFERENCES languages(id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users DROP FOREIGN KEY(interface_language)');
+        $this->addSql('ALTER TABLE users DROP COLUMN interface_language');
+    }
+}
